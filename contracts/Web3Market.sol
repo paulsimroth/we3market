8b09f21de688305a36a56bcb853b031a6b8bc64a
Paul Simroth <paul.simroth@proton.me>
2023-01-18 13:59:36 +0100

    first tests

--- contracts/Web3Market.sol
+++ contracts/Web3Market.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-contract Web3Market {}
+contract Web3Market {
+    string public name;
+    address public owner;
+
+    constructor() {
+        name = "Web3Market";
+        owner = msg.sender;
+    }
+}
--- test/Web3Market.js
+++ test/Web3Market.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
@@ -6,4 +7,28 @@ const tokens = (n) => {
 
 describe("Web3Market", () => {
 
-})
+  let web3market;
+  let deployer, buyer;
+
+  beforeEach(async () => {
+    //Setup Accounts
+    [deployer, buyer] = await ethers.getSigners();
+    console.log("Deployer:", deployer.address, "||", "Buyer:", buyer.address);
+    
+    //Deploy Contracts
+    const Web3Market = await ethers.getContractFactory("Web3Market")
+    web3market = await Web3Market.deploy()
+  })
+
+  describe("Deployment", () => {
+
+    it("Sets owner", async () => {
+      expect(await web3market.owner().to.equal(deployer.address))
+    })
+
+    it("has a name", async () => {
+      const name = await web3market.name()
+      expect(name).to.equal("Web3Market")
+    }) 
+  })
+});
