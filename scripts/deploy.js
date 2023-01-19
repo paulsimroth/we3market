71df377bf0f23ecfa6f6362bb77c736de36a4d66
Paul Simroth <paul.simroth@proton.me>
2023-01-19 16:47:41 +0100

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,8 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat")
+const { ethers } = require("hardhat")
+const hardhat = require("hardhat")
 const { items } = require("../src/items.json")
 
 const tokens = (n) => {
@@ -12,6 +13,34 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //Accounts
+  const [deployer] = await ethers.getSigners()
+
+  //Deploy Web3Market
+  const Web3M = await hardhat.ethers.getContractFactory("Web3Market")
+  const web3m = await Web3M.deploy()
+  await web3m.deployed()
+
+  //After deployment log contract address
+  console.log(`Deployed the Web3 Market contract at: ${web3m.address}\n`);
+
+  //List items for shop
+  for (let i = 0; i < items.length; i++) {
+    const tx = await web3m.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await tx.wait()
+
+    //After listing log item to console
+    console.log(`Item listed ${items[i].id}: ${items[i].name}\n`);
+  }
 
 }
 
