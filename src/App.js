a6630c6a94be2847db377f5861ed2079ee045a94
Paul Simroth <paul.simroth@proton.me>
2023-01-19 17:19:12 +0100

    accounts

--- src/App.js
+++ src/App.js
@@ -13,14 +13,25 @@ import Web3Market from './abis/Web3Market.json';
 import config from './config.json';
 
 function App() {
+  //useState
+  const [account, setAccount] = useState(null);
 
+  //Loading items from blockchain
   const loadChainData = async () => {
-    console.log("Loading...");
-  }
+    const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
 
+  //use Effect calls loading of items
+  useEffect(() => {
+    loadChainData()
+  }, []);
+
+  //Main page
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount}/>
       <h2>Welcome to Web3 Market</h2>
 
     </div>
